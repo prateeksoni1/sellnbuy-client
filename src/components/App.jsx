90333ad121ff2087d7381270c072c784bbf4b390
Adithya.Viswanathan <adithya.viswanathan@ibm.com>
2021-03-18 18:00:31 +0530

    Login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './Dashboard';
 import Home from './Home';
 import Signin from './Signin';
 import Signup from './Signup';
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path='/dashboard' component={Dashboard} />
         <Route path='/signin' component={Signin} />
         <Route path='/signup' component={Signup} />
         <Route path='/' component={Home} exact />
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
-const Signin = () => {
-  return <div>signin</div>;
+const Signin = ({ history }) => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = async (values) => {
+    const res = await axios.post("http://localhost:8000/api/v1/users/login", values);
+    if(res.data.ok === true && res.data.token!=null ){
+      history.push("/dashboard");
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="mt-5 container" style={{ maxWidth: "30vw" }}>
+        <h3 className="display-5 mb-4">Sign In</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter your mail"
+              ref={register({
+                required: true,
+              })}
+            />
+            <p className="text-danger">
+              {errors.email && "Invalid Email provided"}
+            </p>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              ref={register({
+                required: true,
+              })}
+            />
+          </div>
+          <button type="submit" className="mt-3 btn btn-primary w-100 btn-lg">
+            Sign In
+          </button>
+          <div className="mb-4">
+          <Link to="/Signup" className="navbar-brand">
+            Not an Existing User ?
+           <br/>Create your free Account here!
+          </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Signin;
